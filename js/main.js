d780e678aa744b4aec2ba478d165ae7f7fe7795f
Chelsea Quindipan <quindipc@gmail.com>
2022-03-17 16:27:16 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-// Select my grid and choose options
+const filter_btns = document.querySelectorAll(".filter-btn");
+
+
+//Change filter buttons on click
+filter_btns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    filter_btns.forEach((button) => button.classList.remove("active"));
+    btn.classList.add("active");
+
+    //filter's the images when button is clicked
+    let filterValue = btn.dataset.filter;
+
+    $(".grid").isotope({ filter: filterValue });
+  })
+);
+
+
+
 $('.grid').isotope({
-    itemSelector: '.grid-item',
-    layoutMode: 'fitRows'
-  });
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
+});
+
