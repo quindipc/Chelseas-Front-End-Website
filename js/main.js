20416bbcf6849391907fafd3786e5e77141b5176
Chelsea Quindipan <quindipc@gmail.com>
2022-03-18 12:08:18 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 const filter_btns = document.querySelectorAll(".filter-btn");
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
 
+//hamburger menu
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+}
+
+hamburger_menu.addEventListener("click", () => {
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
 
 //Change filter buttons on click
 filter_btns.forEach((btn) =>
